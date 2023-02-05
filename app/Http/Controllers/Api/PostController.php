eb4bc5a350d70c4e6e6bf31cdf4eea67a8216460
mosayeh99 <mohamedelsayeh66@gmail.com>
2023-02-06 00:45:47 +0200

    upload files lap4

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::paginate(5);
+        $posts = Post::with('user')->paginate(5);
         return PostResource::collection($posts);
     }
 
